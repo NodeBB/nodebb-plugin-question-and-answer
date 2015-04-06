58e31146d194b2e62f824e76be854ccac49f4c40
psychobunny <psycho.bunny@hotmail.com>
2015-04-06 16:29:37 -0400

    ability to mark thread as solved/unsolved and question/thread

--- library.js
+++ library.js
@@ -2,6 +2,8 @@
 
 var plugin = {},
 	async = module.parent.require('async'),
+	topics = module.parent.require('./topics'),
+	db = module.parent.require('./database'),
 	SocketPlugins = module.parent.require('./socket.io/plugins');
 
 plugin.init = function(params, callback) {
@@ -31,8 +33,8 @@ plugin.getTopics = function(data, callback) {
 	var topics = data.topics;
 
 	async.map(topics, function(topic, next) {
-		if (topic.isQuestion) {
-			if (topic.isSolved) {
+		if (parseInt(topic.isQuestion, 10)) {
+			if (parseInt(topic.isSolved, 10)) {
 				topic.title = '<span class="answered"><i class="fa fa-question-circle"></i> Solved</span> ' + topic.title;
 			} else {
 				topic.title = '<span class="unanswered"><i class="fa fa-question-circle"></i> Unsolved</span> ' + topic.title;
@@ -46,12 +48,14 @@ plugin.getTopics = function(data, callback) {
 };
 
 plugin.addThreadTool = function(data, callback) {
-	if (data.topic.isQuestion) {
-		data.tools.concat([
+	var isSolved = parseInt(data.topic.isSolved, 10);
+
+	if (parseInt(data.topic.isQuestion, 10)) {
+		data.tools = data.tools.concat([
 			{
-				class: 'toggleSolved',
-				title: data.topic.isSolved ? 'Mark as Unsolved' : 'Mark as Solved',
-				icon: 'fa-check'
+				class: 'toggleSolved ' + (isSolved ? 'alert-warning' : 'alert-success'),
+				title: isSolved ? 'Mark as Unsolved' : 'Mark as Solved',
+				icon: isSolved ? 'fa-question-circle' : 'fa-check-circle'
 			},
 			{
 				class: 'toggleQuestionStatus',
@@ -61,10 +65,10 @@ plugin.addThreadTool = function(data, callback) {
 		]);	
 	} else {
 		data.tools.push({
-			class: 'toggleQuestionStatus',
+			class: 'toggleQuestionStatus alert-warning',
 			title: 'Ask as question',
 			icon: 'fa-question-circle'
-		})
+		});
 	}
 	
 	callback(false, data);
@@ -78,14 +82,63 @@ function handleSocketIO() {
 	SocketPlugins.QandA = {};
 
 	SocketPlugins.QandA.toggleSolved = function(socket, data, callback) {
-		user.isAdministrator(socket.uid, function(err, isAdmin) {
-			if (isAdmin) {
-				plugin.check(callback);
-			}
+		topics.getTopicField(data.tid, 'isSolved', function(err, isSolved) {
+			isSolved = parseInt(isSolved, 10) === 1;
+			console.log(isSolved);
+
+			async.parallel([
+				function(next) {
+					topics.setTopicField(data.tid, 'isSolved', isSolved ? 0 : 1, next);
+				},
+				function(next) {
+					if (!isSolved) {
+						db.sortedSetRemove('topics:unsolved', data.tid, next);
+					} else {
+						db.sortedSetAdd('topics:unsolved', Date.now(), data.tid, next);
+					}
+				}
+			], function(err) {
+				callback(err, {isSolved: !isSolved});
+			});
 		});
 	};
 
-	//SocketPlugins.QandA.
+	SocketPlugins.QandA.toggleQuestionStatus = function(socket, data, callback) {
+		topics.getTopicField(data.tid, 'isQuestion', function(err, isQuestion) {
+			isQuestion = parseInt(isQuestion, 10) === 1;
+
+			async.parallel([
+				function(next) {
+					topics.setTopicField(data.tid, 'isQuestion', isQuestion ? 0 : 1, next);
+				},
+				function(next) {
+					if (!isQuestion) {
+						db.sortedSetAdd('topics:unsolved', Date.now(), data.tid, next);
+					} else {
+						db.sortedSetRemove('topics:unsolved', data.tid, next);
+					}
+				}
+			], function(err) {
+				callback(err, {isQuestion: !isQuestion});
+			});
+		});	
+	};
 }
+
+/*
+categoriesController.recent = function(req, res, next) {
+	var stop = (parseInt(meta.config.topicsPerList, 10) || 20) - 1;
+	topics.getTopicsFromSet('topics:recent', req.uid, 0, stop, function(err, data) {
+		if (err) {
+			return next(err);
+		}
+
+		data['feeds:disableRSS'] = parseInt(meta.config['feeds:disableRSS'], 10) === 1;
+		data.rssFeedUrl = nconf.get('relative_path') + '/recent.rss';
+		data.breadcrumbs = helpers.buildBreadcrumbs([{text: '[[recent:title]]'}]);
+		res.render('recent', data);
+	});
+};
+*/
 
 module.exports = plugin;
--- static/lib/main.js
+++ static/lib/main.js
@@ -1,5 +1,32 @@
 "use strict";
 
-(function() {
+/*global socket*/
 
-}());
+$('document').ready(function() {
+	$(window).on('action:ajaxify.end', function(err, data) {
+		if (data.url.match(/^topic\//)) {
+			addHandlers();
+		}
+	});
+
+	function addHandlers() {
+		$('.toggleQuestionStatus').on('click', toggleQuestionStatus);
+		$('.toggleSolved').on('click', toggleSolved);
+	}
+
+	function toggleQuestionStatus() {
+		var tid = ajaxify.variables.get('topic_id');
+		socket.emit('plugins.QandA.toggleQuestionStatus', {tid: tid}, function(data) {
+			ajaxify.refresh();
+			app.alertSuccess(data.isQuestion ? 'Topic has been marked as a question' : 'Topic is now a regular thread');
+		});
+	}
+
+	function toggleSolved() {
+		var tid = ajaxify.variables.get('topic_id');
+		socket.emit('plugins.QandA.toggleSolved', {tid: tid}, function(data) {
+			ajaxify.refresh();
+			app.alertSuccess(data.isSolved ? 'Topic has been marked as solved' : 'Topic has been marked as unsolved');
+		});
+	}
+});
