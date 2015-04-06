06e1bb214d606edda75f837136102fcfc99534ee
psychobunny <psycho.bunny@hotmail.com>
2015-04-06 15:32:10 -0400

    front end logic

--- library.js
+++ library.js
@@ -1,7 +1,8 @@
 "use strict";
 
 var plugin = {},
-	async = module.parent.require('async');
+	async = module.parent.require('async'),
+	SocketPlugins = module.parent.require('./socket.io/plugins');
 
 plugin.init = function(params, callback) {
 	var app = params.router,
@@ -10,6 +11,8 @@ plugin.init = function(params, callback) {
 
 	app.get('/admin/plugins/question-and-answer', middleware.admin.buildHeader, renderAdmin);
 	app.get('/api/admin/plugins/question-and-answer', renderAdmin);
+
+	handleSocketIO();
 
 	callback();
 };
@@ -28,16 +31,61 @@ plugin.getTopics = function(data, callback) {
 	var topics = data.topics;
 
 	async.map(topics, function(topic, next) {
-		topic.title = '<span class="answered"><i class="fa fa-question-circle"></i> Answered</span> ' + topic.title;
+		if (topic.isQuestion) {
+			if (topic.isSolved) {
+				topic.title = '<span class="answered"><i class="fa fa-question-circle"></i> Solved</span> ' + topic.title;
+			} else {
+				topic.title = '<span class="unanswered"><i class="fa fa-question-circle"></i> Unsolved</span> ' + topic.title;
+			}
+		}
+		
 		return next(null, topic);
 	}, function(err, topics) {
 		return callback(err, data);
 	});
 };
 
+plugin.addThreadTool = function(data, callback) {
+	if (data.topic.isQuestion) {
+		data.tools.concat([
+			{
+				class: 'toggleSolved',
+				title: data.topic.isSolved ? 'Mark as Unsolved' : 'Mark as Solved',
+				icon: 'fa-check'
+			},
+			{
+				class: 'toggleQuestionStatus',
+				title: 'Make this a normal topic',
+				icon: 'fa-comments'
+			}
+		]);	
+	} else {
+		data.tools.push({
+			class: 'toggleQuestionStatus',
+			title: 'Ask as question',
+			icon: 'fa-question-circle'
+		})
+	}
+	
+	callback(false, data);
+};
 
 function renderAdmin(req, res, next) {
 	res.render('admin/plugins/question-and-answer', {});
+}
+
+function handleSocketIO() {
+	SocketPlugins.QandA = {};
+
+	SocketPlugins.QandA.toggleSolved = function(socket, data, callback) {
+		user.isAdministrator(socket.uid, function(err, isAdmin) {
+			if (isAdmin) {
+				plugin.check(callback);
+			}
+		});
+	};
+
+	//SocketPlugins.QandA.
 }
 
 module.exports = plugin;
