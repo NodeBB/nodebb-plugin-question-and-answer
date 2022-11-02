60bf6fd1793cb02c18a2d9ec2ddf99b101272313
Barış Soner Uşaklı <barisusakli@gmail.com>
2022-11-02 09:36:45 -0400

    Merge pull request #96 from yariplus/patch-1
    alerts

--- static/lib/main.js
+++ static/lib/main.js
@@ -8,7 +8,7 @@ $('document').ready(function () {
 	}
 	function alertType(type, message) {
 		require(['alerts'], function (alerts) {
-			alert[type](message);
+			alerts[type](message);
 		});
 	}
 	$(window).on('action:ajaxify.end', function () {
